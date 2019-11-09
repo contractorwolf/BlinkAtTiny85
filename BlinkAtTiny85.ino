394ccea9f343ffa6aea8094810a3393393622b6a
james wolf <contractorwolf@hotmail.com>
2019-11-08 21:37:50 -0500

    add lightning effects updated timing

--- BlinkAtTiny85.ino
+++ BlinkAtTiny85.ino
@@ -2,6 +2,9 @@
 int led = 4;
 int potentiometer = 3;
 int spacer =100;
+int offset = 50;
+
+int count = 0;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
@@ -11,27 +14,50 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   int val = analogRead(potentiometer);
-  val = map(val, 0, 1023, 0, 10000);
+  val = map(val, 0, 1023, 0, 45000);
 
+  if(count%2==0){
     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(spacer+50);              // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(spacer-50);
+    delay(spacer+offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer-offset);
     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(spacer-50);              // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(spacer+50);
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(spacer-50);              // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(spacer);
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(spacer+100);              // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer-offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer+offset);
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer-offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer);
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer+offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer);
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer+offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  }else{
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer-offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer+offset);
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer+offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer-offset);
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer+offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(spacer);
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(spacer-offset);              // wait for a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  }
 
-
-  
   delay(val);
+  count = count + 1;
 
-  // wait for a second
+  if(count > 2000){
+    count = 0;
+  }
 }
